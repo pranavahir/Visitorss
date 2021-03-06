f359ad6778896c590ec5fd11ec33edc06b9785e2
pranavahir <46396023+pranavahir@users.noreply.github.com>
2021-03-06 15:27:15 +0530

    Delete MERNLEC2 directory

--- MERNLEC2/Todo/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Routers from './Router'
-export default function App(){
-  return(
-    <>
-    <Routers/>
-    </>
-  )
-}
--- MERNLEC2/config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: `mongodb+srv://Dashdub:Peace@$0nu@cluster0.9ulqv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
-}
